addd1c9702a1f78eaa8444b9ba0c80dd88125d0e
Armando Méndez <amendezm2009@gmail.com>
2026-02-19 17:51:10 -0600

    feat: desarrollo del index page con links de cv's para descarga

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistemon</title>
+</head>
+<body>
+    <h1>Bienvenido al Sistemón</h1>
+    <br>
+    <img src="./imgteam.png" width="150px" height="150px" alt="super team">
+    <br>
+    <h2>Scrum team</h2>
+    <ul>
+        <li>
+            <a href="./eduardo.docx">Descargar CV de Eduardo Carmona</a>
+        </li>
+        <li>
+            <a href="./carlos.docx">Descargar CV de Carlos Domínguez</a>
+        </li>
+        <li>
+            <a href="./barbara.docx">Descargar CV de Bárbara Roblero</a>
+        </li>
+    </ul>
+</body>
+</html>
